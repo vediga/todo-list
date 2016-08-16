6d45523d18a4d29b91ff3e0122b4e3755fbc2e62
Vikas Ediga <vediga@vmware.com>
2016-08-15 22:34:24 -0700

    [src][client/server] Moved static files to client
    - Moved public/ from server to client and added a default index.html
    - Renamed routes/ to api/
    - Added a globals.js to share global variables across server

--- src/code/main/server/app.js
+++ src/code/main/server/app.js
@@ -5,13 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var users = require('./api/users');
+var globals = require('./shared/globals');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, globals.publicDir + 'views'));
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
@@ -20,9 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// NOTE: serves index.html by default under public directory
+app.use(express.static(path.join(__dirname, globals.publicDir)));
+
 app.use('/users', users);
 
 // catch 404 and forward to error handler
---
+++ src/code/main/server/shared/globals.js
@@ -0,0 +1,6 @@
+// NOTE: All paths are relative to server/app.js
+var globals = {
+   publicDir: '../client/public/'
+};
+
+module.exports = globals;
